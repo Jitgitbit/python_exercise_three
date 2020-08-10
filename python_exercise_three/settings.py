332edc9a8edc5d4bc38ff433a8de4d6003ea2827
thierryD <rowowolf@gmail.com>
2020-08-10 12:04:24 +0000

    Removed old Stuff

--- python_exercise_three/settings.py
+++ python_exercise_three/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&rl_w)&s*)l#ep=y(u(4vv%0r5*fbygtio-_5#o1d7hsu9o1wc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thierryD.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
